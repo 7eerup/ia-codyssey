ec8e08b320d5ed89c476380bc4234461b3d4a806
7eerup <7eerup@gmail.com>
2025-07-20 20:50:09 +0900

    4

--- 04_test/minmax_calculator.py
+++ 04_test/minmax_calculator.py
@@ -1,19 +1,11 @@
 def main():
     try:
         # 입력 값 받고 기준 분리
-<<<<<<< HEAD
         numbers = input("숫자를 공백으로 구분해서 입력하세요.").split()
         # 문자열 float 변환
         float_numbers = [float(num) for num in numbers]
     except ValueError:
         print("잘못된 입력입니다.")
-=======
-        numbers = input("숫자를 공백으로 구분해서 입력하세요: ").split()
-        # 문자열 float 변환
-        float_numbers = [float(num) for num in numbers]
-    except ValueError:
-        print("Invalid input.")
->>>>>>> a23dd09 (ia)
         return
     
     minimum = float_numbers[0]
@@ -26,11 +18,7 @@ def main():
         if num > maximum:
             maximum = num
 
-<<<<<<< HEAD
     print(f"최소값: {minimum}, 최대값: {maximum}")
-=======
-    print(f"Min: {minimum}, Max: {maximum}")
->>>>>>> a23dd09 (ia)
 
 if __name__ == "__main__":
     main()
